11663398ea773e68ac58dddf281e0821e438bb06
cashely <290119516@qq.com>
2020-02-18 17:38:57 +0800

    fix: 腾讯云证书

---
+++ admin/autoRoutes/https.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    uri: '/admin.verlantum.cn',
+    method: 'get',
+    mark: '腾讯云admin.verlantum.cn证书',
+    callback: (req, res) => {
+      res.send('202002170916344ozqi3dlkk6ujihf68692nx5ezmsji3drr81i8x3yzgv70xnnt')
+    }
+  },
+  {
+    uri: '/api.verlantum.cn',
+    method: 'get',
+    mark: '腾讯云api.verlantum.cn证书',
+    callback: (req, res) => {
+      res.send('202002170913470dzkxg8quoybdofu5qodmerkhya9mnus7hlcisw4d1sevosmq3')
+    }
+  }
+]
