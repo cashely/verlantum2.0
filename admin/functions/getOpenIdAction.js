b33c7a1f291f1fee2138e49aa0dffb1ed367469e
cashely <290119516@qq.com>
2021-05-02 02:41:34 +0800

    fix: bug

--- admin/functions/getOpenIdAction.js
+++ admin/functions/getOpenIdAction.js
@@ -4,6 +4,7 @@ module.exports = (code) => {
   return new Promise((resolve, reject) => {
     request({url:`https://api.weixin.qq.com/sns/oauth2/access_token?appid=${wxAppId}&secret=${wxAppSecret}&code=${code}&grant_type=authorization_code`,method:'GET'},function(err,response,body){
       if(!err && response.statusCode == 200){
+          console.log('获取openId的body', body)
           resolve(JSON.parse(body).openid)
       }else {
         console.log(err)
