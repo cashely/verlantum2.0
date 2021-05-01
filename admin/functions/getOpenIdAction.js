7ec0073a5ccf9a006e8f3bf43c1bd5de8b8b1673
cashely <290119516@qq.com>
2021-05-02 02:28:15 +0800

    fix: bug

--- admin/functions/getOpenIdAction.js
+++ admin/functions/getOpenIdAction.js
@@ -1,4 +1,5 @@
 const request = require('request');
+const { wxAppId, wxAppSecret} = require('../config.global');
 module.exports = (code) => {
   return new Promise((resolve, reject) => {
     request({url:`https://api.weixin.qq.com/sns/oauth2/access_token?appid=${wxAppId}&secret=${wxAppSecret}&code=${code}&grant_type=authorization_code`,method:'GET'},function(err,response,body){
