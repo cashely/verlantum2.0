8d9750ffce902da008b537dc82422c0c72f0e03d
cashely <290119516@qq.com>
2021-05-03 10:53:16 +0800

    fix: bug

--- admin/functions/wxPayV3.js
+++ admin/functions/wxPayV3.js
@@ -1,14 +1,15 @@
 const request = require('request');
 const Payment = require('wxpay-v3');
 const path = require('path');
+const fs = require('fs');
 const { wxAppId, wxMchId } = require('../config.global');
-
+console.log(fs.readFileSync(path.resolve(__dirname, '../1472079802_20200424_cert/apiclient_cert.pem')).toString())
 const payment = new Payment({
   appid: wxAppId,
   mchid: wxMchId,
-  private_key: require('fs').readFileSync(path.resolve(__dirname, '../1472079802_20200424_cert/apiclient_cert.pem')).toString(), //或者直接复制证书文件内容
+  private_key: fs.readFileSync(path.resolve(__dirname, '../1472079802_20200424_cert/apiclient_cert.pem')).toString(), //或者直接复制证书文件内容
   serial_no:'55B518F71AFB2E5A99AABED154BEC0D99DEA9216',
-  apiv3_private_key:'773ADDFE99B6749A16D6B9E266F8A30A',
+  apiv3_private_key:'773ADDFE99B6749A16D6B9E266F8A20A',
   notify_url: 'https://api.verlantum.cn/auth/wxpaycallback',
 })
 module.exports = {
