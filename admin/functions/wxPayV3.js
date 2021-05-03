c9c030ada260c0a510090287a20795ea1a03504a
cashely <290119516@qq.com>
2021-05-03 11:04:00 +0800

    fix: bug

--- admin/functions/wxPayV3.js
+++ admin/functions/wxPayV3.js
@@ -7,8 +7,8 @@ console.log(fs.readFileSync(path.resolve(__dirname, '../1472079802_20200424_cert
 const payment = new Payment({
   appid: wxAppId,
   mchid: wxMchId,
-  private_key: fs.readFileSync(path.resolve(__dirname, '../1472079802_20200424_cert/apiclient_cert.pem')).toString(), //或者直接复制证书文件内容
-  serial_no:'55B518F71AFB2E5A99AABED154BEC0D99DEA9216',
+  private_key: fs.readFileSync(path.resolve(__dirname, '../1472079802_20210503_cert/apiclient_key.pem')).toString(), //或者直接复制证书文件内容
+  serial_no:'7E9E0047261197C96D82A3FD70E9A2E2B47AD027',
   apiv3_private_key:'773ADDFE99B6749A16D6B9E266F8A20A',
   notify_url: 'https://api.verlantum.cn/auth/wxpaycallback',
 })
