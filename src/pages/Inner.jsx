da0aedcfd8397fdc642bb3b99d3e084134204021
cashely <290119516@qq.com>
2022-08-23 16:55:32 +0800

    fix: 订单展示contact字段

--- src/pages/Inner.jsx
+++ src/pages/Inner.jsx
@@ -237,7 +237,7 @@ export default class Inner extends Component {
       },
       {
         title: '代理商',
-        render: d => d.agent ? d.agent.title : '——',
+        render: d => d.agent ? d.agent.contact : '——',
       },
       {
         title: '创建时间',
