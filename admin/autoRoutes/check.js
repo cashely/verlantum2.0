d8f2f8c7c7d1bd4c0bd74811721b110713593928
cashely <290119516@qq.com>
2022-08-26 14:41:57 +0800

    add: 增加扫描检测盒

---
+++ admin/autoRoutes/check.js
@@ -0,0 +1,44 @@
+const moment = require('moment');
+const models = require('../model.js');
+const getOpenIdAction = require('../functions/getOpenIdAction');
+const { order, paysignjsapifinal, configSign } = require('../functions/wxPayV3');
+const wxpay = require('../functions/wxpay');
+const { wxMchId, wxAppId } = require('../config.global');
+const generatorOrder = require('../functions/generatorOrder');
+module.exports = [
+  {
+    uri: '/scan-bot',
+    method: 'get',
+    mark: '扫描检测盒页面',
+    async callback(req, res) {
+      const openid = req.cookies.openid;
+      if (!openid) {
+        res.redirect(`/wxcode/get?uri=https://api.verlantum.cn/wxcode/login?redirect=https://api.verlantum.cn/check/scan-bot`);
+      } else {
+        res.render('scan-bot');
+      }
+    }
+  },
+  {
+    uri: '/scan-bot/:botNumber',
+    method: 'post',
+    mark: '录入检测盒',
+    async callback(req, res) {
+      const { openid } = req.cookies;
+      if (!openid) {
+        res.response(500, '获取授权失败,请刷新页面')
+      } else {
+        const { botNumber } = req.params;
+        const conditions = {
+          openid,
+          botNumber
+        };
+        new models.customs(conditions).save().then(() => {
+          req.response(200, 'ok')
+        }).catch(err => {
+          req.response(500, err)
+        })
+      }
+    }
+  }
+]
--- admin/models/check.js
+++ admin/models/check.js
@@ -5,17 +5,45 @@
 const mongoose = require('../db.config');
 
 const Check = new mongoose.Schema({
-  title: {
+  // 检测编号
+  boxNumber: {
     type: String,
     required: true
   },
-  value: {
+  reportPath: { // 报告地址
     type: String,
-    required: true
+  },
+  // 用户id
+  openId: {
+    type: String,
   },
   mark: {
     type: String,
     required: true
+  },
+  // 姓名
+  uname: {
+    type: String,
+  },
+  // 性别
+  sex: {
+    type: String,
+  },
+  // 年龄
+  age: {
+    type: Number
+  },
+  // 省份证号
+  passPortNumber: {
+    type: String
+  },
+  // 采样日期
+  checkDate: {
+    type: Date,
+  },
+  // 手机号
+  phone: {
+    type: String
   }
 }, {
   timestamps: {
