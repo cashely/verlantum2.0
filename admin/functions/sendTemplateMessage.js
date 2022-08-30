bd8b48c7e2f0acf732354c10a07ea265b4d8474a
cashely <290119516@qq.com>
2022-08-30 17:04:48 +0800

    fix: bug

--- admin/functions/sendTemplateMessage.js
+++ admin/functions/sendTemplateMessage.js
@@ -7,16 +7,18 @@ const uri = `https://api.weixin.qq.com/cgi-bin/message/template/send?access_toke
 module.exports = async (openid, messageDate) => {
   try {
     const access_token = await accessToken();
-    request({
-      url: `${uri}${access_token}`,
-      method: 'post',
-      body: JSON.stringify({ touser: openid, ...messageDate }),
-      headers: { 'Content-Type': 'application/json' },
-    }, (err, response, body) => {
-      if (!err && response.statusCode == 200) {
-        return '模版消息发送成功'
-      }
-      return '模版消息发送失败'
+    return new Promise((resolve, reject) => {
+      request({
+        url: `${uri}${access_token}`,
+        method: 'post',
+        body: JSON.stringify({ touser: openid, ...messageDate }),
+        headers: { 'Content-Type': 'application/json' },
+      }, (err, response, body) => {
+        if (!err && response.statusCode == 200) {
+          resolve('模版消息发送成功');
+        }
+        resolve('模版消息发送失败');
+      });
     })
   } catch (err) {
     console.log(err);
