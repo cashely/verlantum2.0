a34c6cf78c05a8b54354fb83ae9d714e84f32187
cashely <290119516@qq.com>
2022-12-13 16:47:08 +0800

    Update excel.js

--- admin/functions/excel.js
+++ admin/functions/excel.js
@@ -1,5 +1,6 @@
 const XLSX = require('xlsx');
 const fs = require('fs');
+const moment = require('moment');
 
 function make_book(data) {
 	var ws = XLSX.utils.aoa_to_sheet(data);
@@ -8,8 +9,23 @@ function make_book(data) {
 	return wb;
 }
 
-module.exports = (data, file) => {
+module.exports = (data, req) => {
+
+	const { filename, } = req.query;
+
+  let downloadPath = path.resolve(__dirname, '..', 'downloads');
+
+	if(!fs.existsSync(downloadPath)) {
+
+		fs.mkdirSync(downloadPath);
+
+	}
+
+	downloadPath = path.resolve(downloadPath, `${filename}-${moment().format('YYYY-MM-DD-hh-mm')}.xls`);
+	
   var wb = make_book(data);
-  XLSX.writeFile(wb, file);
-  return file;
+
+  XLSX.writeFile(wb, downloadPath);
+
+  return downloadPath;
 }
