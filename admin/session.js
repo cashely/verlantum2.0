1de65d14eac2140bc92aaa02a077b8073b541afd
cashely <290119516@qq.com>
2022-12-15 12:56:52 +0800

    Update session.js

--- admin/session.js
+++ admin/session.js
@@ -7,7 +7,7 @@ module.exports = function() {
     saveUninitialized: true,
     cookie: {
       secure: false,
-      expires: 1000 * 60 * 60
+      expires: 1000 * 60 * 60 * 12
     },
     store: MongoStore.create({
       mongoUrl: 'mongodb://127.0.0.1:27017/verlantum',
