d7c5da625872d3da9907c5a5044f87249323b3bd
cashely <290119516@qq.com>
2023-01-06 21:56:49 +0800

    Update generatorOrder.js

--- admin/functions/generatorOrder.js
+++ admin/functions/generatorOrder.js
@@ -3,6 +3,7 @@ const bignumber = require('bignumber.js');
 module.exports = ({aid, price, ratio, good, address, phone, sex, birthday, guardian, isRequireTicket, ticketHead, card, count = 1, username, goodId, payChannel}) => {
   const orderNo = String(Date.now());
   const paymentAmount = new bignumber(count).multipliedBy(price).toNumber();
+  console.log(paymentAmount, '<<<<<----下单付款金额')
   const order = {
     price,
     paymentAmount,
